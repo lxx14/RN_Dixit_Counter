6110fd615056915ccb5ea609d377d9297185a7e6
AlexeyIlchenko <alexeyi@wizardsdev.com>
2019-09-28 16:25:59 +0300

    add animation to transition in routing

--- src/components/navigation/StackNavigation/mainFlowStackNavigator.js
+++ src/components/navigation/StackNavigation/mainFlowStackNavigator.js
@@ -4,12 +4,28 @@ import { createStackNavigator } from 'react-navigation-stack';
 import MainScreen from '../../screens/MainScreen/MainScreen';
 import SettingsScreen from '../../screens/SettingsScreen/SettingsScreen';
 import StatisticScreen from '../../screens/StatisticScreen/StatisticScreen';
+import { commonNavigationOptions } from './navigationOptions';
 
 const mainFlowCtackNavigator = createStackNavigator(
     {
-        Main: MainScreen,
-        Settings: SettingsScreen,
-        Statistic: StatisticScreen,
+        Main: {
+            screen: MainScreen,
+            navigationOptions: () => ({
+                ...commonNavigationOptions,
+            })
+        },
+        Settings: {
+            screen: SettingsScreen,
+            navigationOptions: () => ({
+                ...commonNavigationOptions,
+            })
+        },
+        Statistic: {
+            screen: StatisticScreen,
+            navigationOptions: () => ({
+                ...commonNavigationOptions,
+            })
+        },
     }
 );
 
---
+++ src/components/navigation/StackNavigation/navigationOptions.js
@@ -0,0 +1,26 @@
+import { Easing, Animated } from 'react-native';
+
+export const commonNavigationOptions = {
+    cardStyle: {
+        backgroundColor: '#FFFFF',
+    },
+    cardShadowEnabled: false,
+    transitionConfig: () => ({
+        transitionSpec: {
+            duration: 500,
+            easing: Easing.out(Easing.poly(4)),
+            timing: Animated.timing,
+            useNativeDriver: true,
+        },
+        screenInterpolator: sceneProps => {
+            const { layout, position, scene } = sceneProps;
+            const thisSceneIndex = scene.index;
+            const width = layout.initWidth;
+            const translateX = position.interpolate({
+                inputRange: [thisSceneIndex - 1, thisSceneIndex],
+                outputRange: [width, 0],
+            })
+            return { transform: [{ translateX }] }
+        },
+    }),
+};
